7e6585d30c63b8e9d9ce5422bf192b4e9b2613af
slee3593 <slee3593@gatech.edu>
2024-01-08 17:44:24 +0900

    add: UI 작성

--- lib/data/subway_repository_impl.dart
+++ lib/data/subway_repository_impl.dart
@@ -9,9 +9,6 @@ class SubwayInfoRepositoryImpl implements SubwayInfoRepository {
   Future<List<SubwayArrivalInfo>> getSubwayInfo(String stationName) async{
     final model = await _api.getSubwayArrivalInfoList(stationName);
     List<SubwayArrivalInfo> infoResult = [];
-    if (infoResult == null) {
-      return [];
-    }
     model.map((e) => infoResult.add(e));
     return infoResult;
   }
--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/subway_repository_impl.dart';
+
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -8,10 +10,35 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final _textController = TextEditingController();
+  final subwayRepository = SubwayInfoRepositoryImpl();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('서울 지하철 근처역 찾기'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              hintText: '역 이름을 입력해주세요',
+            ),
+          ),
+          ListView(children: [
 
+          ]),
+          // ListView.builder(itemBuilder: (context, index)  {
+          //   final subwayList = await subwayRepository.getSubwayInfo(_textController.text);
+          //   return ListTile(
+          //     title: subwayList[index]
+          //   );
+          //
+          // },)
+        ],
+      ),
     );
   }
 }
